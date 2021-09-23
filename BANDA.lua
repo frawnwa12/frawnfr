63daf8d8387f56d3617facacd241a826f02002e2
frawnwa12 <90830942+frawnwa12@users.noreply.github.com>
2021-09-23 18:52:51 +0200

    Add files via upload

--- BANDA.lua
+++ BANDA.lua
@@ -4602,11 +4602,11 @@ end
 if text == 'التواصل'  then
 database:del(bot_id..'Srt:Bot') 
 local Text = [[ 
-[TWL](t.me/m_a_r_i_os)
+[TWL](t.me/source_frawn)
 ]] 
 keyboard = {}  
 keyboard.inline_keyboard = { 
-{{text = 'ͲᎳՏΌᏞ', url="t.me/m_a_r_i_os"}}, 
+{{text = 'ͲᎳՏΌᏞ', url="t.me/source_frawn"}}, 
 } 
 local msg_id = msg.id_/2097152/0.5 
 https.request("https://api.telegram.org/bot"..token..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/vagwg/7&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
