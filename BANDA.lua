a322365a51fff6a17439c8a850ab5a9503c89f9b
frawnwa12 <90830942+frawnwa12@users.noreply.github.com>
2021-09-27 21:24:32 +0200

    Add files via upload

--- BANDA.lua
+++ BANDA.lua
@@ -13304,27 +13304,9 @@ end
 tdcli_function ({ID = "SearchPublicChat",username_ = username}, start_function, nil)
 end
 
-if text == "رتبتي" then
-local msg_id = msg.id_/2097152/0.5  
-local textt = ' 🌚💘 رتبتك هي  '⤌  '..Rutba(msg.sender_user_id_,msg.chat_id_)
-keyboard = {} 
-keyboard.inline_keyboard = {
-{
-{text = ' الـمـطـور ',url="http://t.me/"..sudos.UserName},
-},
-{
-{text = ' اضف البوت الي مجموعتك '  ,url="t.me/"..dofile("./Banda.lua").botUserName.."?startgroup=start"},
-},
-}
-local function getpro(extra, result, success) 
-if result.photos_[0] then 
-https.request("https://api.telegram.org/bot"..token.. /sendPhoto?chat_id=  .. msg.chat_id_ ..  &photo= ..result.photos_[0].sizes_[1].photo_.persistent_id_.. &caption=  .. URL.escape(textt).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard)) 
-else 
-send(msg.chat_id_, msg.id_,textt, 1,  md ) 
-end 
-end 
-tdcli_function ({ ID = "GetUserProfilePhotos", user_id_ = msg.sender_user_id_, offset_ = 0, limit_ = 1 }, getpro, nil) 
-return false
+if text ==  رتبتي  then
+local rtp = Rutba(msg.sender_user_id_,msg.chat_id_)
+send(msg.chat_id_, msg.id_,  🌚💘 رتبتك هي  ⤌  ..rtp)
 end
 
 if text == 'انا مين' and SudoBot(msg) then
